e9d06ca7cc35f8d92deef80cfe36876aa2c89445
Denbau <den.bau@mail.ru>
2019-02-25 11:41:10 +0400

    Init

---
+++ bin/setup.sh
@@ -0,0 +1,79 @@
+# DOCKER SETUP
+# DOCKER SETUP
+
+sudo yum check-update
+
+curl -fsSL https://get.docker.com/ | sh
+
+sudo systemctl start docker
+
+sudo systemctl status docker
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker $(whoami)
+
+sudo docker run hello-world
+
+# docker run -it centos
+
+# --- ALTERNATE INSTALL ---
+
+# sudo yum install -y yum-utils \
+#   device-mapper-persistent-data \
+#   lvm2
+
+# sudo yum-config-manager \
+#     --add-repo \
+#     https://download.docker.com/linux/centos/docker-ce.repo
+
+# sudo yum install docker-ce docker-ce-cli containerd.io
+
+# yum list docker-ce --showduplicates | sort -r
+
+# sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+
+# sudo systemctl start docker
+
+# sudo docker run hello-world
+
+# --- ALTERNATE INSTALL ---
+
+
+
+# INSTALL DOCKER-COMPOSE
+# INSTALL DOCKER-COMPOSE
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+
+
+# INSTALL GIT
+# INSTALL GIT
+
+sudo dnf install git-all
+
+git --version
+
+
+
+# GIT CLONE COMPOSER STARTER KIT
+# GIT CLONE COMPOSER STARTER KIT
+
+git clone https://github.com/libgit2/libgit2
+
+chmod +x ./bin/start.sh
+
+
+
+# START DOCKER UP
+# START DOCKER UP
+
+cd /bin/
+start.sh
+
+# curl https://gist.githubusercontent.com/Bkndns/c445e066c00a0b8beb863b3413e4a09a/raw/390e8d09183850a9a68155d4ad5b7a993fe6b676/setup-command.sh > qsetup.sh
