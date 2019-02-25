ec7d3e8abaa390c32490a6f8629139216dc400fe
Denbau <den.bau@mail.ru>
2019-02-25 15:40:57 +0400

    New .sh files

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,19 +1,19 @@
 # DOCKER SETUP
 # DOCKER SETUP
 
-sudo yum check-update
+yum check-update
 
 curl -fsSL https://get.docker.com/ | sh
 
-sudo systemctl start docker
+systemctl start docker
 
-sudo systemctl status docker
+systemctl status docker
 
-sudo systemctl enable docker
+systemctl enable docker
 
-sudo usermod -aG docker $(whoami)
+usermod -aG docker $(whoami)
 
-sudo docker run hello-world
+docker run hello-world
 
 # docker run -it centos
 
@@ -44,9 +44,9 @@ sudo docker run hello-world
 # INSTALL DOCKER-COMPOSE
 # INSTALL DOCKER-COMPOSE
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
 
@@ -55,7 +55,7 @@ docker-compose --version
 # INSTALL GIT
 # INSTALL GIT
 
-sudo dnf install git-all
+dnf install git-all
 
 git --version
 
@@ -73,7 +73,7 @@ chmod +x ./bin/start.sh
 # START DOCKER UP
 # START DOCKER UP
 
-cd /bin/
+cd ./bin/
 start.sh
 
 # curl https://gist.githubusercontent.com/Bkndns/c445e066c00a0b8beb863b3413e4a09a/raw/390e8d09183850a9a68155d4ad5b7a993fe6b676/setup-command.sh > qsetup.sh
